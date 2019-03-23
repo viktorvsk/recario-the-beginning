486c8fbfcab40ddaf0f150d52b47b0e350b2a598
Viktor Vsk <me@viktorvsk.com>
2019-03-23 20:30:10 +0200

    Fix for Android

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import React from "react";
 import {Provider} from "react-redux";
 import configureStore from "./store/configureStore";
 
+import {Font} from "expo";
+
+
 import Root from "./Root";
 
 const store = configureStore();
 
 export default class App extends React.Component {
+
+    async componentWillMount () {
+        await Font.loadAsync({Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")});
+    }
 
     render () {
         return (
